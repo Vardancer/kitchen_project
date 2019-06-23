2b5548964ab6a36a1ea14ed7cefb9805c2642091
vardancer <vardancer@gmail.com>
2019-06-23 10:44:52 +0300

    seems got understood django queries (((

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,5 +1,5 @@
-from kitchen.models import Week, Order, Dish, OrdersTransit
-from django.forms import Form, ModelMultipleChoiceField, CheckboxSelectMultiple
+from kitchen.models import Week, Order, Dish, OrdersTransit, Day
+from django.forms import Form, ModelMultipleChoiceField, CheckboxSelectMultiple, CheckboxInput, Select
 
 
 class OrderForm(Form):
@@ -8,48 +8,45 @@ class OrderForm(Form):
         self.user = kwargs.pop('user')
         super(OrderForm, self).__init__(*args, **kwargs)
 
-        for day in self.week.day.all():
-            queryset = day.dish.all()
+        days = Day.objects.filter(week=self.week).prefetch_related('week__day')
+        dishes = Dish.objects.prefetch_related('days__dish').filter(days__week=self.week)
+        # print(dishes.query)
+        for d in days:
+            print(d.slug)
+            queryset = dishes.filter(days=d)
+            print(queryset)
+            # if queryset:
+            #     field_kwargs = {
+            #         'label': d,
+            #         'queryset': queryset,
+            #         'widget': CheckboxSelectMultiple,
+            #     }
+            #     self.fields.update({
+            #         d.slug: ModelMultipleChoiceField(**field_kwargs)
+            #     })
 
-            if queryset:
-                field_kwargs = {
-                    'label': day,
-                    'queryset': queryset,
-                    'widget': CheckboxSelectMultiple,
-                }
-                if self.initial.get(day.slug):
-                    field_kwargs.update({
-                        'initial': self.initial.get(day.slug)
-                    })
-                self.fields.update({
-                    day.slug: ModelMultipleChoiceField(**field_kwargs)
-                })
+        # for day in self.week.day.all():
+        #     queryset = day.dish.all()
+        #     # print(queryset)
+        #     if queryset:
+        #         field_kwargs = {
+        #             'label': day,
+        #             'queryset': queryset,
+        #             'widget': CheckboxSelectMultiple,
+        #         }
+        #         if self.initial.get(day.slug):
+        #             field_kwargs.update({
+        #                 'initial': self.initial.get(day.slug)
+        #             })
+        #         self.fields.update({
+        #             day.slug: ModelMultipleChoiceField(**field_kwargs)
+        #         })
 
-    def save(self):
-        pass
-        q = Order.objects.filter(
-            user=self.user,
-            week=self.week
-        )
-        # print(q.query)
-        if q:
-            print("111")
-        else:
-            print("222")
-            total_cost = 0
-
-            # prices = []
-            for m in self.week.day.all():
-                response = self.cleaned_data.get(m.slug)
-                """sasasa"""
-                for pr in response:
-                    total_cost += pr.price
-
-            ordr, o_log = Order.objects.get_or_create(user=self.user, week=self.week, total_cost=total_cost)
-            for m in self.week.day.all():
-                response = self.cleaned_data.get(m.slug)
-                for pr in response:
-                    rdtr = OrdersTransit.objects.create(dish_id=pr.id, order=ordr, cost=pr.price, is_half=False)
-                    print(rdtr)
-
-        return ordr
+    # def save(self):
+    #     days = Day.objects.prefetch_related(self.week)
+    #     for day in days:
+    #         # print(day)
+    #         resp = self.cleaned_data.get(day.slug)
+    #         # ordr, ordrlog = Order.objects.get_or_create()
+    #         print(resp)
+    #     # ord, ordlog = Order.objects.get_or_create(week=self.week, user=self.user)
